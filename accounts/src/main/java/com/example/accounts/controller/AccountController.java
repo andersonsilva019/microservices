a5c67f04ea5ae8c9429f697d62d0d4c0f89aa3c8
Anderson Silva <andersonnsilva@alu.ufc.br>
2025-05-10 20:57:28 -0300

    inital commit

---
+++ accounts/src/main/java/com/example/accounts/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.example.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World! 2";
+    }
+}
