9424a5542091f708591419c275ffacda1d3468ea
Anderson Silva <andersonnsilva@alu.ufc.br>
2025-05-13 21:55:50 -0300

    Enhancing documentation of REST APIs using @OpenAPIDefinition

--- accounts/src/main/java/com/example/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/example/accounts/AccountsApplication.java
@@ -1,9 +1,36 @@
 package com.example.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Anderson Silva",
+						email = "and@and.com",
+						url = "https://www.example.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.example.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Brandon Browners Accounts microservice REST API Documentation",
+				url = "https://localhost:8080/swagger-ui.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- accounts/src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
@@ -36,9 +36,6 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with given number "+customerDTO.getMobileNumber());
         }
 
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
-
         Customer savedCustomer = customerRepository.save(customer);;
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -55,8 +52,7 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
+
         return newAccount;
     }
 
