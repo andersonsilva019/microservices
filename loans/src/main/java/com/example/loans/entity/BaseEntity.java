519069c868833b055a7422c9844a6a02a409a4f9
Anderson Silva <andersonnsilva@alu.ufc.br>
2025-05-19 21:56:44 -0300

    Create initial structure of Loans microservice

---
+++ loans/src/main/java/com/example/loans/entity/BaseEntity.java
@@ -0,0 +1,14 @@
+package com.example.loans.entity;
+
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
+}
